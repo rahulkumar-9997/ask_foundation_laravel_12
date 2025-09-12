2855c9e31cc2dbac5873071a9b4b061d3a2d75ae
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2025-09-12 16:07:43 +0530

    12_09_25_pages_create

--- resources/views/frontend/pages/focus-area/bone-health.blade.php
+++ resources/views/frontend/pages/focus-area/bone-health.blade.php
@@ -16,7 +16,218 @@
         </div>
     </div>
 </div>
-
+<div class="our-approach">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="our-approach-box-content bone-health">
+                    <div class="our-approach-content">
+                        <div class="section-title">
+                            <h3 class="wow fadeInUp">Strong Bones, Strong Future</h3>
+                            <h2 class="text-anime-style-2" data-cursor="-opaque">
+                                Building awareness for healthier bones, joints, and active lives.
+                            </h2>
+                        </div>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            Bones are the foundation of movement and independence. Yet, bone health is often ignored until it becomes a problem. Through the Bone Health & Orthopaedics initiative, ASK Foundation works to raise awareness, support early diagnosis, and provide guidance that helps people stay strong and active at every stage of life.
+                        </p>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            Bone-related issues are no longer confined to the elderly. Sedentary lifestyles, poor nutrition, and lack of awareness are causing younger individuals to suffer from arthritis, fractures, and osteoporosis. Simple habits—posture correction, calcium-rich diets, and regular exercise-can prevent years of pain and disability.
+                        </p>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            ASK Foundation organizes Bone Health Awareness Programs to educate communities, especially students and young professionals, on the importance of preventive care. Through these sessions, participants learn how early habits—like walking, stretching, or ergonomic seating-can protect them from severe problems later in life.
+                        </p>
+                    </div>
+                    <div class="our-approach-image">
+                        <figure class="image-anime">
+                            <img src="{{asset('fronted/assets/ask-img/focus-area/bone-health-and-orthopedics.jpg')}}" alt="Strong Bones, Strong Future">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="our-features choose-area focus-area-oth">
+    <div class="container">
+        <div class="row section-row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2 class="text-anime-style-2" data-cursor="-opaque">
+                        Our Key Focus Areas
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/bone-health-awareness.svg')}}" alt="Bone Health Awareness">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Bone Health Awareness </h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Workshops and sessions on posture, exercise, and nutrition to make bone health simple and practical for all age groups.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/orthopaedic-care.svg')}}" alt="Orthopaedic Care">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Orthopaedic Care </h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Free camps for early detection of osteoporosis, arthritis, and spinal issues before they become severe.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/joint-replacement-guidance.svg')}}" alt="Joint Replacement Guidance">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Joint Replacement Guidance </h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Expert tips on exercise, diet, and daily habits to keep bones and joints strong in everyday life.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/community-health-drives.svg')}}" alt="Community Health Drives">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Community Health Drives </h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Helping underprivileged patients access essential orthopaedic care without financial barriers.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="our-features">
+    <div class="container">
+        <div class="row section-row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2 class="text-anime-style-2" data-cursor="-opaque">
+                        Why Bone Health Matters
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-1 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/mobility-and-independence.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Mobility & Independence</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>Strong bones and joints allow people to move freely and live without limitations.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-1 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/active-ageing.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Active Ageing</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>With awareness and care, older adults can maintain an active, fulfilling lifestyle.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-1 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/preventing-disability-preventing-disability.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Preventing Disability</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>Early awareness and treatment reduce the risk of fractures, deformities, and long-term disability.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="how-we-help mb-6">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="how-we-help-content">
+                    <div class="section-title">
+                        <h2 class="text-anime-style-2" data-cursor="-opaque">
+                            Leadership & Expertise
+                        </h2>                        
+                    </div>
+                    <div class="how-we-help-body wow fadeInUp" data-wow-delay="0.4s">
+                        <p class="wow fadeInUp" data-wow-delay="0.2s" style="color: #fff;">With over 5,000 knee and hip replacements and 200+ complex revision surgeries to his credit, Dr. Anne is one of India’s most experienced orthopaedic surgeons. His career spans advanced training in India, the UK, and the USA, equipping him with expertise in cutting-edge techniques like computer-navigated surgery, long-lasting “Oxinium” implants, pelvi-acetabular reconstructions, and limb salvage in bone tumor patients. But beyond his achievements in the operating theater, what defines him is his determination to take bone health education and care to the grassroots.</p>
+                    </div>
+                    <div class="how-we-help-btn wow fadeInUp" data-wow-delay="0.6s">
+                        <a href="{{ route('contact-us')}}" class="btn-default">Join as a Volunteer</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="how-help-list">
+                    <div class="how-help-item wow fadeInUp">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Learn & Share</h3>
+                            <p>Attend awareness programs and spread knowledge about bone health with your family, friends, and community.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.2s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Volunteer Your Time</h3>
+                            <p>Join as a volunteer at bone health camps and events to support screenings, awareness drives, and education programs.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.4s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Partner with Us</h3>
+                            <p>Collaborate with ASK Foundation to host programs at schools, colleges, workplaces, or community centers.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.6s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Support Access to Care</h3>
+                            <p>Contribute resources or donations to help underprivileged patients receive timely diagnosis and treatment.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- How We Help Section End -->
 @endsection
 @push('scripts')
 
--- resources/views/frontend/pages/focus-area/road-safety.blade.php
+++ resources/views/frontend/pages/focus-area/road-safety.blade.php
@@ -16,7 +16,223 @@
         </div>
     </div>
 </div>
-
+<div class="our-approach">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="our-approach-box-content bone-health">
+                    <div class="our-approach-content">
+                        <div class="section-title">
+                            <h3 class="wow fadeInUp">Road Safety is Everyone’s Responsibility</h3>
+                            <h2 class="text-anime-style-2" data-cursor="-opaque">
+                                Building safer communities through awareness, education, and responsible driving.
+                            </h2>
+                        </div>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            Every year, countless families are impacted by road accidents—many of which could have been prevented. Most victims are young individuals in their most productive years, leaving behind devastating emotional and financial consequences for their families. ASK Foundation’s <strong>Road Safety Program</strong> is dedicated to spreading awareness and encouraging safe road practices that save lives.
+                        </p>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            Road safety is not just about following traffic rules—it is about respecting life, valuing discipline, and practicing empathy on the road. Accidents happen in seconds, but the impact can last a lifetime. At ASK Foundation, we believe education and awareness are the keys to prevention.
+                        </p>
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">
+                            Through structured programs in schools, colleges, workplaces, and communities, we aim to transform the way people think about road safety. Whether it’s wearing a helmet, using a seatbelt, avoiding distractions, or giving way to others, small actions can make a big difference.
+                        </p>
+                    </div>
+                    <div class="our-approach-image">
+                        <figure class="image-anime">
+                            <img src="{{asset('fronted/assets/ask-img/focus-area/road-safety/road-safety.jpg')}}" alt="Strong Bones, Strong Future">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="our-features choose-area focus-area-oth">
+    <div class="container">
+        <div class="row section-row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2 class="text-anime-style-2" data-cursor="-opaque">
+                        Our Key Focus Areas
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/road-safety/our-key/youth-awareness.svg')}}" alt="Youth Awareness">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Youth Awareness</h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                           Interactive sessions in schools and colleges teaching safe driving, helmets, and seatbelt use.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/road-safety/our-key/community-campaigns.svg')}}" alt="Community Campaigns">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Community Campaigns</h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Workshops, rallies, and awareness drives promoting patience, discipline, and safe road habits.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/road-safety/our-key/corporate-sessions.svg')}}" alt="Corporate Sessions">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Corporate Sessions</h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                           Programs for employees to encourage responsible commuting and safer travel habits.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="how-help-item wow fadeInUp">
+                    <div class="icon-box about-ask-icon-bg1 about-ask-foundation-icon-container">
+                        <img src="{{asset('fronted/assets/ask-img/focus-area/road-safety/our-key/practical-safety-education.svg')}}" alt="Practical Safety Education">
+                    </div>
+                    <div class="how-help-item-content">
+                        <h3>Practical Safety Education</h3>
+                    </div>
+                    <div class="highlight-con">
+                        <p>
+                            Actionable guidance on mobile-free driving, rest breaks, and reducing accident risks.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="our-features">
+    <div class="container">
+        <div class="row section-row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2 class="text-anime-style-2" data-cursor="-opaque">
+                        Why Road Safety Matters
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-0 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/mobility-and-independence.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Protecting Young Lives</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>
+                                Many accident victims are in the 18–50 age group, leaving behind dependents.
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-0 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/active-ageing.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Reducing Preventable Loss</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>
+                                Most accidents happen due to neglect of basic safety measures.
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="solution-card block group rounded-xl overflow-hidden relative aos-init aos-animate">
+                    <div class="absolute left-0 top-0 h-full w-full -z-10 bg-black-50 lg-opacity-0 group-hover-opacity-100"></div>
+                    <img src="{{asset('fronted/assets/ask-img/focus-area/why-bone/preventing-disability-preventing-disability.jpg')}}" alt="Fintech" loading="lazy" width="552" height="840" class="why-bone-img w-full max-h-[450px] object-cover relative -z-20">
+                    <div class="flex flex-col justify-between gap-10 text-white absolute top-0 left-0 w-full h-full p-6 z-0">
+                        <h4 class="text-2xl font-semibold text-center">Creating Responsible Citizens</h4>
+                        <div class="solution-content lg-opacity-0 group-hover-opacity-100 text-center">
+                            <p>Road safety is about building a culture of respect, patience, and empathy.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="how-we-help mb-6">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="how-we-help-content">
+                    <div class="section-title">
+                        <h2 class="text-anime-style-2" data-cursor="-opaque">
+                            Our Approach
+                        </h2>                        
+                    </div>
+                    <div class="how-we-help-body wow fadeInUp" data-wow-delay="0.4s">
+                        <p class="wow fadeInUp" data-wow-delay="0.2s" style="color: #fff;">
+                            At ASK Foundation, we emphasize that road safety is not about winning on the road—it is about saving lives. The simple act of yielding the right of way can prevent tragedies. By shifting mindsets from competition to compassion, we can create safer roads for everyone.
+                        </p>
+                    </div>
+                    <div class="how-we-help-btn wow fadeInUp" data-wow-delay="0.6s">
+                        <a href="{{ route('contact-us')}}" class="btn-default">Join as a Volunteer</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="how-help-list">
+                    <div class="how-help-item wow fadeInUp">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Learn & Share</h3>
+                            <p>Attend awareness programs and spread knowledge about bone health with your family, friends, and community.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.2s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Volunteer Your Time</h3>
+                            <p>Join as a volunteer at bone health camps and events to support screenings, awareness drives, and education programs.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.4s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Partner with Us</h3>
+                            <p>Collaborate with ASK Foundation to host programs at schools, colleges, workplaces, or community centers.</p>
+                        </div>
+                    </div>
+                    <div class="how-help-item wow fadeInUp" data-wow-delay="0.6s">
+                        
+                        <div class="how-help-item-content">
+                            <h3>Support Access to Care</h3>
+                            <p>Contribute resources or donations to help underprivileged patients receive timely diagnosis and treatment.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 @push('scripts')
 
